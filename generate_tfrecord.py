fb2627b9aa280f6e2ae2d7ff952b4844e355f1f8
wagonhelm <alliseesolutions@gmail.com>
2017-10-16 22:48:17 -0600

    added files

---
+++ generate_tfrecord.py
@@ -0,0 +1,88 @@
+# Adapted from
+# https://github.com/datitran/raccoon_dataset/blob/master/generate_tfrecord.py
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import io
+import pandas as pd
+import tensorflow as tf
+
+from PIL import Image
+from object_detection.utils import dataset_util
+from collections import namedtuple, OrderedDict
+
+
+# Add more class labels as needed, make sure to start at 1
+def class_text_to_int(row_label):
+    if row_label == 'wPawn':
+        return 1
+    if row_label == 'bPawn':
+        return 2 
+    else:
+        None
+
+def split(df, group):
+    data = namedtuple('data', ['filename', 'object'])
+    gb = df.groupby(group)
+    return [data(filename, gb.get_group(x)) for filename, x in zip(gb.groups.keys(), gb.groups)]
+
+
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(class_text_to_int(row['class']))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def main(_):
+    for i in ['test', 'train']:
+        writer = tf.python_io.TFRecordWriter(i+'.record')
+        path = os.path.join(os.getcwd(), 'images/'+i)
+        examples = pd.read_csv('data/'+i+'.csv')
+        grouped = split(examples, 'filename')
+        for group in grouped:
+            tf_example = create_tf_example(group, path)
+            writer.write(tf_example.SerializeToString())
+        writer.close()
+        print('Successfully created the '+i+ ' TFRecords')
+
+
+if __name__ == '__main__':
+    tf.app.run()
