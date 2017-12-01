f2f9ec9051290758cf7e5bb34a1716fba9cf6b2d
Atabak Dehban <atabakd@gmail.com>
2017-12-01 20:24:20 +0000

    changes

---
+++ python/create_train_img_annotation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 18:17:13 2017
+
+@author: atabak
+"""
+
+from __future__ import absolute_import, print_function, division
+import os
+from PIL import Image
+
+home = os.path.expanduser('~/')
+target_path = home + 'detection_raw_data/'
+input_mess_path = home + 'SHAPES2017'
